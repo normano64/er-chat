3a5b3af8a142e4267a513e5ea3db2786f857aa78
Mattias <mattias.linder87@gmail.com>
2014-04-25 11:52:50 +0200

    join

--- src/server.erl
+++ src/server.erl
@@ -44,25 +44,29 @@ command(Socket, <<78,73,67,75>>, NewNick, User) -> % NICK
     gen_tcp:send(Socket, [<<58>>, User, <<32>>, list_to_binary("NICK"), <<32>>, NewNick, <<10>>]),
     io:format("< NICK ~s~n", [NewNick]),
     NewNick;
-command(Socket, <<85,83,69,82>>, Message, User) -> % USER
+command(Socket, <<85,83,69,82>>, _Message, User) -> % USER
     gen_tcp:send(Socket, [<<48, 48, 49, 32, 58>>, User, <<10>>]),
     gen_tcp:send(Socket, [<<58>>, User, <<32>>, list_to_binary("Welcome to the Internet Relay Network"), <<32>>, User, <<10>>]),
     io:format("< Welcome to the Internet Relay Network ~s~n", [User]),
     User;
-command(Socket, <<77,79,68,69>>, Message, User) -> % MODE
+command(Socket, <<77,79,68,69>>, _Message, User) -> % MODE
     gen_tcp:send(Socket, [<<58>>, User, <<32>>, list_to_binary("MODE"), <<32>>, User, <<32>>, list_to_binary("+i\n")]),
     io:format("< MODE pebe7501 +i~n"),
     User;
-command(Socket, <<87,72,79,73,83>>, Message, User) -> % WHOIS
+command(Socket, <<87,72,79,73,83>>, _Message, User) -> % WHOIS
     gen_tcp:send(Socket, [<<58>>, User, <<32>>, list_to_binary("WHOIS pebe7501 :Per Bergqwist\n")]),
     io:format("< WHOIS pebe7501 :Per Bergqwist~n"),
     User;
-command(Socket, <<80,73,78,71>>, Message, User) -> % PING send PONG
+command(Socket, <<80,73,78,71>>, _Message, User) -> % PING send PONG
     gen_tcp:send(Socket, [<<58>>, User, <<32>>, list_to_binary("PONG localhost localhost\n")]),
     gen_tcp:send(Socket, [<<58>>, list_to_binary("bitflip"), <<32>>, list_to_binary("PRIVMSG"), <<32>>, User, <<32>>, <<58>>, list_to_binary("Hoppas det smakar massor med ost\n")]),
     io:format("< PONG localhost localhost~n"),
     User;
-command(Socket, <<>>, Message, User) -> % QUIT
+command(Socket,<<74,79,73,78>>, Message, User)-> %JOIN message = #detduskrivit
+    gen_tcp:send(Socket,[<<51,51,49,32>>,Message,<<32,58>>,list_to_binary("No Topic Is Set\n")]),
+    gen_tcp:send(Socket,[<<58>>,User, <<74,79,73,78>>, Message,<<10>>]),
+    io:format("Who is knoking on my door???~n");
+command(_Socket, <<>>, _Message, User) -> % QUIT
     User;
 command(_, _, _, User) ->
     User.
