550941b35cefa0b8e991845fb2caff6a1f9313be
TomasWallin <tomaszync@gmail.com>
2014-05-06 12:24:52 +0200

    Added parser_test to parser.erl

--- src/parser.erl
+++ src/parser.erl
@@ -1,9 +1,15 @@
 -module(parser).
-%% -include_lib("eunit/include/eunit.hlr").
+%%-include_lib("eunit/include/eunit.hlr").
 %%-compile(export_all).
 -export([parse/1, loop/2]).
 -define(SLASHR,13).
 -define(SPACE,32).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                                           %
+%                                  Parser functions                                         %
+%                                                                                           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 loop(UserPid,_OtherPid)->
     receive {ok,Message}-> 
@@ -14,8 +20,25 @@ loop(UserPid,_OtherPid)->
     end.
 
 parse(Bitstring) ->
-    Bitstring_nor = << <<Bitstring_nor>> || <<Bitstring_nor>> <= Bitstring, Bitstring_nor =/= ?SLASHR>>, %%removes all /r
+    Bitstring_nor = << <<Bitstring_nor>> || <<Bitstring_nor>> <= Bitstring, Bitstring_nor =/= ?SLASHR>>,
     Bitlist = binary:split(Bitstring_nor, <<?SPACE>>, [global]),
     [Command|Parameters] = Bitlist,
     {Command,Parameters}.
 
+%%Notice that the current implementation cannot handle multiple spaces following eachother and will generate empty parameters.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                                           %
+%                               EUnit parser test                                           %
+%                                                                                           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+parser_test() ->
+    Bin1 = <<72,?SLASHR,69,?SLASHR,76,?SLASHR,76,?SLASHR,79>>,
+    Bin2 = <<72,69,?SPACE,76,76,79>>,
+    Bin3 = <<72,?SPACE,69,?SPACE,76,?SPACE,76,?SPACE,79>>,
+    Bin4 = <<?SLASHR,72,69,?SLASHR,?SPACE,76,76,?SLASHR,79,?SPACE>>,
+    ?assertEqual(parser(Bin1),{<<"HELLO">>,[]}),
+    ?assertEqual(parser(Bin2),{<<"HE">>,[<<"LLO">>]}),
+    ?assertEqual(parser(Bin3),{<<"H">>,[<<"E">>,<<"L">>,<<"L">>,<<"O">>]}),
+    ?assertEqual(parser(Bin4),{<<"HE">>,[<<"LL">>,<<"O">>,<<>>]}).
