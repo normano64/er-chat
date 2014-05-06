814d40ddd2a020e99a2776066040ddb5bf43b8c0
TomasWallin <tomaszync@gmail.com>
2014-05-06 11:30:38 +0200

    Finnished the parser core functionality

--- src/parser.erl
+++ src/parser.erl
@@ -1,35 +1,21 @@
 -module(parser).
 %% -include_lib("eunit/include/eunit.hlr").
 %%-compile(export_all).
--export([parse/1, bitstring/0]).
+-export([parse/1, loop/2]).
 -define(SLASHR,13).
 -define(SPACE,32).
 
-split_Bitlist(Bitlist_without_r) ->
-    split_Bitlist(Bitlist_without_r, [], []).
-split_Bitlist([], Acc, Word) ->
-    lists:reverse([Word|Acc]);
-split_Bitlist([Head|Tail], Acc, Word) ->
-    if	
-	Head =:= ?SPACE -> NewWord = lists:reverse(Word),
-	    split_Bitlist([Tail], [NewWord|Acc], []);
-	Head =/= ?SPACE -> split_Bitlist([Tail], Acc, [Head|Word]);
-	true -> ok
+loop(UserPid,_OtherPid)->
+    receive {ok,Message}-> 
+	    case parse(Message) of
+		{<<"NICK">>,List}-> 
+		    UserPid ! {nick,List},
+		    loop(UserPid,_OtherPid) end
     end.
 
-
-
 parse(Bitstring) ->
-    Bitlist = bitstring_to_list(Bitstring),
-    Bitlist_without_r = [X || X <- Bitlist, X =/= ?SLASHR],
-    io:fwrite(Bitlist_without_r), %%Testing the output.
-    Split_Bitlist = [X || [X] <- Bitlist_without_r, X =/= ?SPACE],
-  %%  Split_Bitlist = split_Bitlist(Bitlist_without_r),
-    io:fwrite(Split_Bitlist), %%Testing the output.
-    ok.
-    
-bitstring() ->
-    Bin1 = <<72,32,69,32,76,76,32,79>>,
-    List = binary_to_list(Bin1),
-    io:fwrite(List),
-    ok.
+    Bitstring_nor = << <<Bitstring_nor>> || <<Bitstring_nor>> <= Bitstring, Bitstring_nor =/= ?SLASHR>>, %%removes all /r
+    Bitlist = binary:split(Bitstring_nor, <<?SPACE>>, [global]),
+    [Command|Parameters] = Bitlist,
+    {Command,Parameters}.
+
