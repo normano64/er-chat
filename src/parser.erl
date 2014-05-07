650b2cae6d9375c9f95cc05f143712ff9a93a013
TomasWallin <tomaszync@gmail.com>
2014-05-07 13:48:27 +0200

    expanded the functionality of parse in parser.erl

--- src/parser.erl
+++ src/parser.erl
@@ -3,7 +3,7 @@
 -module(parser).
 -include_lib("eunit/include/eunit.hrl").
 %%-compile(export_all).
--export([parse/1, loop/2]).
+-export([parse/1,loop/2]).
 -define(SLASHR,13).
 -define(SPACE,32).
 -define(COLON,58).
@@ -17,36 +17,48 @@
 loop(UserPid,OtherPid)->
     receive {ok,Message}-> 
 	    case parse(Message) of
-		{<<"NICK">>,List}-> 
+		{_,<<"NICK">>,List}-> 
 		    OtherPid ! {nick,List},
 		    loop(UserPid,OtherPid);
-		{<<"USER">>,List} ->
+		{_,<<"USER">>,List} ->
 		    UserPid ! {user,List},
 		    loop(UserPid,OtherPid)
 		end
     end.
 
-%% @doc The parse function takes a binary string and formates it to a tuple {Command/binary, [Parameters/binary]}.
+%% @doc The parse function takes a binary string and formates it to a tuple {Prefix/binary, Command/binary, [Parameters/binary]}.
 %%
 %%== Example ==
 %% parse(<<"USER GUEST 0 * :Ronald Mcdonald">>)
-%% {<<"USER">>,[<<"guest">>,<<"0">>,<<"*">>,<<":Ronald Mcdonald">>]}
+%% {noprefix, <<"USER">>,[<<"guest">>,<<"0">>,<<"*">>,<<":Ronald Mcdonald">>]}
 %%
 %%WEAKNESSES OF PARSE
 %%It cannot handle multiple spaces following eachother and will then generate empty parameters <<>>.
 %%If a colon isn't preceded by a space it will not be placed in Colonpart.
 %%Everything after " :" will be placed in the Colonpart and will not acted upon anymore, even another " :".
-
-
-
+%%Handling of prefix is not complete and bugfree.
 
 parse(Bitstring) ->
-    Bitstring_nor = << <<Bitstring_nor>> || <<Bitstring_nor>> <= Bitstring, Bitstring_nor =/= ?SLASHR>>,
-    [Bitstring_noColon|Colonpart] = binary:split(Bitstring_nor, <<?SPACE,?COLON>>, [global]),
+%%Remove Carriage Return.
+    Bitstring_noR = << <<Bitstring_noR>> || <<Bitstring_noR>> <= Bitstring, Bitstring_noR =/= ?SLASHR>>,
+%%Trims of the prefix.
+    case Bitstring_noR of
+	<<$:, Rest/binary>> ->
+	    {Pos,_} = binary:match(Rest,<<?SPACE>>),
+	    <<Prefix:Pos/binary, Unused:1/binary, Bitstring_noPrefix/binary>> = Rest;
+	_ ->
+	    Prefix = noprefix,
+	    Bitstring_noPrefix = Bitstring_noR
+	end,
+    %%Splits the Bitstring at the Colonpart if it exists.
+    [Bitstring_noColon|Colonpart] = binary:split(Bitstring_noPrefix, <<?SPACE,?COLON>>, [global]),
+    %%Splits the rest at spaces.
     Bitlist = binary:split(Bitstring_noColon, <<?SPACE>>, [global]),
+    %%Appends all the parts and return.
     Bitlist_complete = Bitlist ++ Colonpart,
     [Command|Parameters] = Bitlist_complete,
-    {Command,Parameters}.
+    {Prefix,Command,Parameters}.
+    
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -61,8 +73,11 @@ parser_test() ->
     Bin3 = <<?SLASHR,72,69,?SLASHR,?SPACE,76,76,?SLASHR,79,?SPACE>>,
     Bin4 = <<"USER guest 0 * :Ronald Mcdonald">>,
     Bin5 = <<"USER guest 0 *:Ronald Mcdonald">>,
-    ?assertEqual(parse(Bin1),{<<"HELLO">>,[]}),
-    ?assertEqual(parse(Bin2),{<<"H">>,[<<"E">>,<<"L">>,<<"L">>,<<"O">>]}),
-    ?assertEqual(parse(Bin3),{<<"HE">>,[<<"LL">>,<<"O">>,<<>>]}),
-    ?assertEqual(parse(Bin4),{<<"USER">>,[<<"guest">>,<<"0">>,<<"*">>,<<":Ronald Mcdonald">>]}),
-    ?assertEqual(parse(Bin5),{<<"USER">>,[<<"guest">>,<<"0">>,<<"*:Ronald">>,<<"Mcdonald">>]}).
+    ?assertEqual(parse(Bin1),{noprefix,<<"HELLO">>,[]}),
+    ?assertEqual(parse(Bin2),{noprefix,<<"H">>,[<<"E">>,<<"L">>,<<"L">>,<<"O">>]}),
+    ?assertEqual(parse(Bin3),{noprefix,<<"HE">>,[<<"LL">>,<<"O">>,<<>>]}),
+    ?assertEqual(parse(Bin4),{noprefix,<<"USER">>,[<<"guest">>,<<"0">>,<<"*">>,<<":Ronald Mcdonald">>]}),
+    ?assertEqual(parse(Bin5),{noprefix,<<"USER">>,[<<"guest">>,<<"0">>,<<"*:Ronald">>,<<"Mcdonald">>]}).
+
+
+
